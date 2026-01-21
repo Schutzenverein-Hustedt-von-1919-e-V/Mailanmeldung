29f6aee53de8ac7f1eb5c34280817d2dada40562
DeKas90 <kaul_dennis@yahoo.de>
2026-01-21 23:22:44 +0100

    Create seatable-proxy.js

---
+++ functions/seatable-proxy.js
@@ -0,0 +1,6 @@
+Mailanmeldung/             ← Root deines Repos
+├─ index.html              ← Dein bestehendes Formular
+├─ functions/              ← Neuer Ordner für Netlify Function
+│   └─ seatable-proxy.js   ← Die Serverless Function
+├─ package.json            ← Node Dependencies für die Function
+└─ README.md               ← Optional
